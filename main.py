a3e1a6800342443529a97b7b5b24857ad20472cd
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-08-13 13:27:00 +0330

    clean title ok

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ ingest_type_to_elastic.apply(objects_ids)
 print("------------------------------------------\nLevel To Elastic\n------------------------------------------")
 ingest_level_to_elastic.apply(objects_ids)
 
-# print("------------------------------------------\nSubject To Elastic\n------------------------------------------")
-# ingest_subject_keyword_to_elastic.apply(objects_ids)
-#
-# print("------------------------------------------\nAll Actors To Elastic\n------------------------------------------")
-# ingest_all_actors_to_elastic.apply(objects_ids)
+print("------------------------------------------\nSubject To Elastic\n------------------------------------------")
+ingest_subject_keyword_to_elastic.apply(objects_ids)
 
-# print("------------------------------------------\nAffected Graph To Elastic\n------------------------------------------")
-# ingest_affected_graph_to_elastic.apply(objects_ids)
+print("------------------------------------------\nAll Actors To Elastic\n------------------------------------------")
+ingest_all_actors_to_elastic.apply(objects_ids)
+
+print("------------------------------------------\nAffected Graph To Elastic\n------------------------------------------")
+ingest_affected_graph_to_elastic.apply(objects_ids)
 
 # print("------------------------------------------\nReferences To Elastic\n------------------------------------------")
 # ingest_references_to_elastic.apply()
@@ -40,5 +40,5 @@ ingest_level_to_elastic.apply(objects_ids)
 # print("------------------------------------------\nClustering To Elastic\n------------------------------------------")
 # ingest_clustering_to_elastic.apply()
 
-# print("------------------------------------------\nVectors To Elastic\n------------------------------------------")
-# ingest_vectors_to_elastic.apply(objects_ids)
+print("------------------------------------------\nVectors To Elastic\n------------------------------------------")
+ingest_vectors_to_elastic.apply(objects_ids)
