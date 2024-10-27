cb3cb362d8ceaabb18d2cd5a45903915a8ea8eaa
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-10-27 10:51:56 +0330

    clean title ok

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ from scripts import (source_to_elastic, ingest_data_to_elastic, ingest_type_to_e
 #
 # print("------------------------------------------\nAffected Graph To Elastic\n------------------------------------------")
 # ingest_affected_graph_to_elastic.apply(objects_ids)
-
-print("------------------------------------------\nReferences To Elastic\n------------------------------------------")
-ingest_references_to_elastic.apply()
+#
+# print("------------------------------------------\nReferences To Elastic\n------------------------------------------")
+# ingest_references_to_elastic.apply()
 
 print("------------------------------------------\nClustering To Elastic\n------------------------------------------")
 ingest_clustering_to_elastic.apply()
