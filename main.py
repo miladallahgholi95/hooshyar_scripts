094430f31837fb9dfed0c8acd2595b84c6149a82
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-10-29 12:33:57 +0330

    clean title ok

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ print("------------------------------------------\nReferences To Elastic\n------
 ingest_references_to_elastic.apply()
 
 print("------------------------------------------\nClustering To Elastic\n------------------------------------------")
-ingest_clustering_to_elastic.apply()
+# ingest_clustering_to_elastic.apply()
 
 # print("------------------------------------------\nVectors To Elastic\n------------------------------------------")
 # ingest_vectors_to_elastic.apply(None)
