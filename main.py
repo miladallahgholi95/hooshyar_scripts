3cee8356c10a3b35091c45aacfa8910569c8700d
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-10-29 13:43:41 +0330

    clean title ok

--- main.py
+++ main.py
@@ -25,19 +25,19 @@ objects_ids = None
 # print("------------------------------------------\nLevel To Elastic\n------------------------------------------")
 # ingest_level_to_elastic.apply(objects_ids)
 #
-# print("------------------------------------------\nSubject To Elastic\n------------------------------------------")
+print("------------------------------------------\nSubject To Elastic\n------------------------------------------")
 ingest_subject_keyword_to_elastic.apply(objects_ids)
-#
-# print("------------------------------------------\nAll Actors To Elastic\n------------------------------------------")
+
+print("------------------------------------------\nAll Actors To Elastic\n------------------------------------------")
 ingest_all_actors_to_elastic.apply(objects_ids)
-#
-# print("------------------------------------------\nAffected Graph To Elastic\n------------------------------------------")
+
+print("------------------------------------------\nAffected Graph To Elastic\n------------------------------------------")
 ingest_affected_graph_to_elastic.apply(objects_ids)
-#
-# print("------------------------------------------\nReferences To Elastic\n------------------------------------------")
+
+print("------------------------------------------\nReferences To Elastic\n------------------------------------------")
 ingest_references_to_elastic.apply()
 
-print("------------------------------------------\nClustering To Elastic\n------------------------------------------")
+# print("------------------------------------------\nClustering To Elastic\n------------------------------------------")
 # ingest_clustering_to_elastic.apply()
 
 # print("------------------------------------------\nVectors To Elastic\n------------------------------------------")
