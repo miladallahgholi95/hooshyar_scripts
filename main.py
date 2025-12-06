52975190de487abb2aa2a4795e541199e524ac06
Milad <miladallahgholi95@gmail.com>
2025-12-06 15:04:30 +0330

    paragraph actor script added

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ from scripts import  ingest_paragraph_actors_to_elastic, ingest_actors_time_seri
     # # ingest_vectors_to_elastic.apply(objects_ids)
 
 ingest_paragraph_actors_to_elastic.apply()
-ingest_actors_time_series_to_elastic.apply()
+# ingest_actors_time_series_to_elastic.apply()
 
 # except Exception as e:
 #     # error_message = f"خطا در اجرای اسکریپت:\n{traceback.format_exc()}"
